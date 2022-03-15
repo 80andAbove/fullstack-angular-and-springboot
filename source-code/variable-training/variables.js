9282390b209cc01112d9bd3c317d800c44321dcd
80andAbove <shogomiyagi94@gmail.com>
2022-03-15 20:41:38 +0900

    playing around with variables

---
+++ source-code/variable-training/variables.js
@@ -0,0 +1,8 @@
+var joyceWright = "Joyce Wright";
+var temp = 23.1;
+var date = "March 15";
+var alive = true;
+console.log(joyceWright);
+console.log(temp);
+console.log(date);
+console.log(alive);
---
+++ source-code/variable-training/variables.ts
@@ -0,0 +1,9 @@
+let joyceWright: string = "Joyce Wright";
+let temp: number = 23.1;
+let date: string = "March 15";
+let alive: boolean = true;
+
+console.log(joyceWright);
+console.log(temp);
+console.log(date);
+console.log(alive);
